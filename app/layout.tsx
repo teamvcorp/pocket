66c4c87303c5760e49c78779120b7aeeb283c538
Robert Von Der Becke <teamvcorp@thevacorp.com>
2026-03-23 16:36:51 -0500

    android logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,14 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "Pocket Jesus",
   },
-  icons: { icon: "/icon.svg", apple: "/icon.svg" },
+  icons: {
+    icon: [
+      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/icon-192.png", sizes: "192x192", type: "image/png" }],
+  },
 };
 
 export const viewport: Viewport = {
